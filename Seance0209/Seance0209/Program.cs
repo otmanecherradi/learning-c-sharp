cbf9031d5ed0cc253c25585a0aa8a35e82c93d27
Otmane <51253168+otmanecherradi@users.noreply.github.com>
2021-02-09 16:07:50 +0100

    ✨ getting to know the langage

---
+++ Seance0209/Seance0209/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seance0209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
