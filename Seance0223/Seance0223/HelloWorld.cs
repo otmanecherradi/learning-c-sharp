e26dac1008ce048d3eb6190a05fcad713d55aa4d
Otmane <51253168+otmanecherradi@users.noreply.github.com>
2021-02-23 15:53:10 +0100

    :sparkles: Intro to OOP and classes

---
+++ Seance0223/Seance0223/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seance0223
+{
+    class HelloWorld
+    {
+        public string Name { get; set; }
+
+        public string salutation = "Hello World";
+
+        public void SayHello()
+        {
+            Console.WriteLine(salutation);
+        }
+
+        public string GetHelloTo()
+        {
+            return $"Hello {Name}";
+        }
+    }
+}
---
+++ Seance0223/Seance0223/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Seance0223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Seance Feb 23rd
+
+            // classes and objects
+
+            HelloWorld helloWorld = new HelloWorld();
+
+            helloWorld.SayHello();
+
+            helloWorld.Name = "World, from C#";
+
+            string salutation = helloWorld.GetHelloTo();
+
+            Console.WriteLine(salutation);
+
+        }
+    }
+}
