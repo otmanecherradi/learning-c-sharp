99a34e2c12fd80bf6da598c221a25721687cd579
Otmane <51253168+otmanecherradi@users.noreply.github.com>
2021-03-04 18:15:09 +0100

    :sparkles: Solution for ArrayList execise

---
+++ Seance0304/Seance0304/GestionCDs.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Seance0304
+{
+    class GestionCDs
+    {
+        private ArrayList cds;
+        public ArrayList CDs
+        {
+            get { return cds; }
+            set { cds = value; }
+        }
+
+        public GestionCDs()
+        {
+            cds = new ArrayList();
+        }
+
+        private bool isPresent(int n)
+        {
+            foreach (CD cD in cds)
+                if (cD.Numero.Equals(n))
+                    return true;
+            return false;
+        }
+
+        public bool AjouterCD(CD cd)
+        {
+            if (isPresent(cd.Numero))
+                return false;
+
+            cds.Add(cd);
+            return true;
+        }
+
+        public bool AjouterCD(int n, string t, Genres g, DateTime d, string a)
+        {
+            if (isPresent(n))
+                return false;
+
+            CD newCD = new CD(n, t, g, d, a);
+            cds.Add(newCD);
+            return true;
+        }
+
+        public bool RechercherCD(int n) => isPresent(n);
+
+        public int RechercherCD(string t)
+        {
+            int r = -1;
+            foreach (CD cD in cds)
+            {
+                if (cD.Titre.Equals(t))
+                    r = 1;
+            }
+            return r;
+        }
+
+        public bool ModifierCD(int n, string t, Genres g, DateTime d, string a)
+        {
+            if (!isPresent(n))
+                return false;
+
+
+            foreach (CD cD in cds)
+            {
+                if (cD.Numero.Equals(n))
+                {
+                    cD.Titre = t;
+                    cD.Genre = g;
+                    cD.DateEdition = d;
+                    cD.Author = a;                    
+                }
+
+            }
+
+            return true;
+        }
+
+        public void AffichierCDs()
+        {
+            foreach (CD cD in cds)
+                Console.WriteLine(cD.ToString());
+        }
+
+    }
+}
---
+++ Seance0304/Seance0304/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Seance0304
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Seance march 4rt
+
+            // testing for CD and GestionCDs classes
+
+            CD cd1 = new CD(1, "title 1", Genres.Rock, DateTime.Now, "author 1");
+            CD cd2 = new CD(2, "title 2", Genres.Pop, DateTime.Now, "author 2");
+
+            GestionCDs gs = new GestionCDs();
+
+            CD[] cds = { cd1, cd2 };
+
+            foreach (CD cd in cds)
+                if (gs.AjouterCD(cd))
+                    Console.WriteLine($"CD {cd.Titre} est ajoute avec succe");
+                else
+                    Console.WriteLine($"CD {cd.Titre} existe deja");
+
+            gs.AffichierCDs();
+
+            Console.WriteLine("\n---------------------------------------------------------------\n");
+
+        }
+    }
+}
