3c97b73f5c10c97bd667aeb9cd53b591475e6c5c
Otmane <51253168+otmanecherradi@users.noreply.github.com>
2021-03-24 20:20:32 +0100

    :sparkles: Abstract Classes and Exercises

---
+++ Seance0324/Seance0324/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Seance0324
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Seance march 24th
+
+            // abstract classes
+
+
+        }
+    }
+}
---
+++ Seance0324/Seance0324/Voiture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seance0324
+{
+    abstract class Voiture
+    {
+        public string Numero { get; set; }
+        public double PrixBase { get; set; }
+
+
+        public Voiture() { }
+
+        public Voiture(string im, double pxb)
+        {
+            Numero = im;
+            PrixBase = pxb;
+        }
+
+
+        public abstract double getPrix();
+
+        public override string ToString()
+        {
+            return Numero;
+        }
+    }
+}
